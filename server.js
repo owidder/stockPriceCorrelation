96238840fc3bdc62945d4f2399de9cc3c8611ac4
Oliver Widder <oliver.widder@gmail.com>
2020-01-01 14:38:03 +0100

    1337 -> 1339

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use('/', express.static(__dirname + '/'));
 
-const port = process.argv.length > 2 ? process.argv[2] : 1337;
+const port = process.argv.length > 2 ? process.argv[2] : 1339;
 const server = require('http').createServer(app);
 
 server.listen(port, function () {
